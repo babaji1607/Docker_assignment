2743805e9972e75392c66a60132a46bbdec24193
babaji1607 <pishach.singh@gmail.com>
2026-09-01 14:50:28 +0530

    final commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const SUBMIT_URL = 'http://127.0.0.1:5000/submit'
+const SUBMIT_URL = process.env.BACKEND_URL
 
 // Add body parsing middleware
 app.use(express.urlencoded({ extended: true }))
